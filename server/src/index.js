b3814f59eec3ace670ad37456e892613c966448c
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2020-03-01 22:50:50 +0100

    server source clean up

--- server/src/index.js
+++ server/src/index.js
@@ -1,29 +1,12 @@
+import 'dotenv/config'
 import { MongoClient } from "mongodb"
 import app, { server } from "./server"
 import MoviesDAO from "./dao/moviesDAO"
 import UsersDAO from "./dao/usersDAO"
 import CommentsDAO from "./dao/commentsDAO"
 
-/**
-Ticket: Connection Pooling
-
-Please change the configuration of the MongoClient object by setting the
-maximum connection pool size to 50 active connections.
-*/
-
-/**
-Ticket: Timeouts
-
-Please prevent the program from waiting indefinitely by setting the write
-concern timeout limit to 2500 milliseconds.
-*/
-
 MongoClient.connect(
   process.env.MFLIX_DB_URI,
-  // TODO: Connection Pooling
-  // Set the poolSize to 50 connections.
-  // TODO: Timeouts
-  // Set the write timeout limit to 2500 milliseconds.
   {
     poolSize: 50,
     wtimeout: 2500,
--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,13 @@
-import 'dotenv/config'
 import express from "express"
 import path from "path"
 import cors from "cors"
 import bodyParser from "body-parser"
 import { ApolloServer, gql } from 'apollo-server-express'
-import movies from "./api/movies.route"
-import users from "./api/users.route"
 
 import { typeDefs, resolvers } from  './schema'
-import MoviesController from './api/movies.controller'
-import CommentsController from './api/comments.controller'
-import UsersController from './api/users.controller'
 import MoviesDAO from './dao/moviesDAO'
 import CommentsDAO from './dao/commentsDAO'
 import UsersDAO from './dao/usersDAO'
-
 
 export const server = new ApolloServer({
   typeDefs,
@@ -27,10 +20,6 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-// Register api routes
-app.use("/api/v1/movies", movies)
-app.use("/api/v1/user", users)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, "../../client/out")))
